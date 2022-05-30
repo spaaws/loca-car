a45552674ddc392e86a7aeca5d5b921b54ad2e80
Wellington <spaaws@gmail.com>
2022-05-30 20:59:20 -0300

    Ajustes

--- connection.php
+++ connection.php
@@ -1,15 +1,10 @@
 <?php
     //Conexão com bando de dados
     function connection(){
-//         $servername = "localhost";
-//         $username   = "root";
-//         $password   = "";
-//         $db         = "locadora";
-        
-        $servername = "u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        $username   = "ccrk1ipe9g6gzwdd";
-        $password   = "ygqpqbs78glzwk73";
-        $db         = "wxa8dxw15yab8254";
+        $servername = HOST;
+        $username   = USERNAME;
+        $password   = PASSWORD;
+        $db         = DATABASE;
 
         try {
         $conn = new PDO("mysql:host=$servername;dbname=$db;charset=utf8", $username, $password);
